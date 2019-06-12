85458307a477bd0cb06ea659c138013c87c7f4cc
zia <zia@softwarealliance.com>
2019-06-12 12:52:13 +0500

    route change

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::group(['middleware' => 'auth'], function (){
 
     Route::resource('drivers', 'DriverController');
-    Route::resource('vehicles', 'VehicleController');
+    Route::resource('/vehicles', 'VehicleController');
 
 	// Route::get('/vehicles', 'VehicleController@index')->name('vehicles');
     // Route::get('/vehicle-add', 'VehicleController@create')->name('vehicle-add');
